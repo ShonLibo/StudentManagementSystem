5048cb7e7b021dfff50cc3b49dc35a5142831832
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-01 14:41:09 +0300

    #RegistrationUpdated

--- student.management.core/src/main/java/mysql/com/Registration.java
+++ student.management.core/src/main/java/mysql/com/Registration.java
@@ -1,11 +1,11 @@
 package mysql.com;
 
-public class Registration {
-    private String email;
-    private String name;
-    private String course;
-    private String gender;
-    private String hobbies;
+public final class Registration {
+    private final String email;
+    private final String name;
+    private final String course;
+    private final String gender;
+    private final String hobbies;
 
     public Registration(String email, String name, String course, String gender, String hobbies) {
         this.email = email;
@@ -15,44 +15,23 @@ public class Registration {
         this.hobbies = hobbies;
     }
 
-    // Getters and setters
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getCourse() {
         return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
     }
 
     public String getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String getHobbies() {
         return hobbies;
-    }
-
-    public void setHobbies(String hobbies) {
-        this.hobbies = hobbies;
     }
 }
