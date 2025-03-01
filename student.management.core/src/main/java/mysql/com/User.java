23b0f03abe303e162f864480872dea6164e785b2
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-01 14:34:55 +0300

    Updated User

--- student.management.core/src/main/java/mysql/com/User.java
+++ student.management.core/src/main/java/mysql/com/User.java
@@ -1,28 +1,19 @@
 package mysql.com;
 
-public class User {
-    private String email;
-    private String password;
+public final class User {
+    private final String email;
+    private final String password;
 
     public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
-    // Getters and setters
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
