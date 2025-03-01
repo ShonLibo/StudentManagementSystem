606681dcac6fd1aee5cd0f04979b807bbb5ef80c
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-01 16:06:40 +0300

    #Dependencies added

--- student.management.ui/src/main/java/module-info.java
+++ student.management.ui/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module student.management.ui {
     requires java.desktop; // For Swing
-    requires student.management.core; // Dependency on core module
-    exports mysql.com; // Export the package containing UI classes
+    requires student.management.core;// Dependency on core module
+    requires java.sql;
+    exports mysql.com;
+    // Export the package containing UI classes
 }
