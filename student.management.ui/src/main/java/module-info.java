c6956b82b931e1431a15002d0a12492cc1b2de06
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-01 16:08:04 +0300

    Update module-info.java

--- student.management.ui/src/main/java/module-info.java
+++ student.management.ui/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module student.management.ui {
     requires java.desktop; // For Swing
     requires student.management.core;// Dependency on core module
-    requires java.sql;
-    exports mysql.com;
-    // Export the package containing UI classes
+    requires java.sql; // Dependency on core module
+    exports mysql.com;// Export the package containing UI classes
 }
