f659fc5b49ac915cd6323f549648e7b1c15e7f30
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-01 14:42:24 +0300

    Update Utils

--- student.management.core/src/main/java/mysql/com/Utils.java
+++ student.management.core/src/main/java/mysql/com/Utils.java
@@ -3,12 +3,15 @@ package mysql.com;
 import javax.swing.*;
 import java.awt.*;
 
-public class Utils {
+public final class Utils {
+    private Utils() {
+        // Private constructor to prevent instantiation
+    }
+
     public static void logError(String message, Exception e) {
         System.err.println(message);
         e.printStackTrace();
         JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
-        System.exit(1);
     }
 
     public static JLabel createLabel(String text) {
