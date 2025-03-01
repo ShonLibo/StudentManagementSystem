88814fc600929d00ae663868bac75688644a0c4e
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-01 16:31:51 +0300

    #Duplicate remove

--- student.management.app/src/main/java/module-info.java
+++ student.management.app/src/main/java/module-info.java
@@ -3,5 +3,6 @@ module student.management.app {
     requires student.management.ui; // Dependency on UI module
     requires com.jtattoo;
     requires java.desktop;
-    requires java.sql; // External library for look and feel
+    requires java.sql;
+    requires JTattoo; // External library for look and feel
 }
